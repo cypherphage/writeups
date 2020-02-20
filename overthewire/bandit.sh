43abc74589bea2328b90745709ed7f7da5e072ae
Aditya Nain <cypherphage@gmail.com>
2020-02-20 14:32:15 +0530

    added level12 details

--- overthewire/bandit.sh
+++ overthewire/bandit.sh
@@ -16,7 +16,7 @@ ls
 cat spaces\ in\ this\ filename
 exit
 
-ssh bandit3@banit.labs.overthewire.org -p 2220
+ssh bandit3@bandit.labs.overthewire.org -p 2220
 UmHadQclWmgdLOKQ3YNgjWxGoRMb5luK
 ls
 cd inhere/
@@ -24,54 +24,59 @@ ls -a
 cat .hidden
 exit
 
-ssh bandit4@banit.labs.overthewire.org -p 2220
+ssh bandit4@bandit.labs.overthewire.org -p 2220
 pIwrPrtPN36QITSp3EQaw936yaFoFgAB
 ls
 cd inhere/
 find . -type f | xargs file | grep "text" | awk '{print substr($1, 1, length($1)-1)}' | xargs cat
 exit
 
-ssh bandit5@banit.labs.overthewire.org -p 2220
+ssh bandit5@bandit.labs.overthewire.org -p 2220
 koReBOKuIDDepwhWk7jZC0RTdopnAYKh
 ls
 cd inhere/
 find . -type f ! -executable -size 1033c | xargs cat
 exit
 
-ssh bandit6@banit.labs.overthewire.org -p 2220
+ssh bandit6@bandit.labs.overthewire.org -p 2220
 DXjZPULLxYr17uwoI01bNLQbtFemEgo7
 cd ..
 cd ..
 find . -type f -user bandit7 -group bandit6 -size 33c 2>&1 | grep -v "Permission denied" | grep -v "No such file" | xargs cat
 exit
 
-ssh bandit7@banit.labs.overthewire.org -p 2220
+ssh bandit7@bandit.labs.overthewire.org -p 2220
 HKBPTKQnIay4Fw76bEy8PVxKEDQRKTzs
 grep "millionth" data.txt | awk '{print $2}'
 exit
 
-ssh bandit8@banit.labs.overthewire.org -p 2220
+ssh bandit8@bandit.labs.overthewire.org -p 2220
 cvX2JJa4CFALtqS87jk27qwqGhBM9plV
 sort data.txt | uniq -u
 exit
 
-ssh bandit9@banit.labs.overthewire.org -p 2220
+ssh bandit9@bandit.labs.overthewire.org -p 2220
 UsvVyFSfZZWbi6wgC7dAFyFuR6jQQUhR
 strings data.txt | grep "[A-Za-z0-9]\{32\}$" | awk '{print $2}'
 exit
 
-ssh bandit10@banit.labs.overthewire.org -p 2220
+ssh bandit10@bandit.labs.overthewire.org -p 2220
 truKLdjsbJ5g7yyJ2X2R0o3a5HQJFuLk
 base64 -d data.txt | awk '{print $4}'
 exit
 
-ssh bandit11@banit.labs.overthewire.org -p 2220
+ssh bandit11@bandit.labs.overthewire.org -p 2220
 IFukwKGsFW8MOq3IRFqrxE1hxTNEbUPR
 cat data.txt | tr 'A-Za-z' 'N-ZA-Mn-za-m' | awk '{print $4}'
 exit
 
-ssh bandit11@banit.labs.overthewire.org -p 2220
+ssh bandit12@bandit.labs.overthewire.org -p 2220
 5Te8Y4drgCRfCx8ugdwuEX8KFC6k2EUu
+check file type
+xxd -r filename
+rename
+decompress using tar, gzip, bzip2
+
 
 
 
